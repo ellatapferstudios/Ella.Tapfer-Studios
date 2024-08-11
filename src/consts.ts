64647e0a2e7389f64354ff19a9956d67cdbee453
Henry Egloff <henryegloff@users.noreply.github.com>
2024-08-11 21:47:41 +1000

    Developing image components

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Astro Sky";
 export const SITE_DESCRIPTION =
-  "An elegant and streamlined Astro theme, built for artists, designers";
+  "Welcome to Astro Sky. An elegant and streamlined Astro theme, built for artists, designers.";
