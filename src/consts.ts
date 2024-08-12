2534904364fc611f24a7193310d4f819233db380
Henry Egloff <henryegloff@users.noreply.github.com>
2024-08-12 16:58:50 +1000

    Minor tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Astro Sky";
 export const SITE_DESCRIPTION =
-  "Welcome to Astro Sky. An elegant and streamlined Astro theme, built for artists, designers.";
+  "Welcome to Astro Sky. A minimal and elegant portfolio theme built with Astro.";
